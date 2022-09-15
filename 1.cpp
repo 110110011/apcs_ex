7bf7efc615f471b577d61a4f9aa8d981d162863e
110110011 <113594506+110110011@users.noreply.github.com>
2022-09-15 18:51:11 +0800

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(void)
+{       
+    printf( "hello 王信元 \n" );
+    printf( "my height is %d cm \n",173);
+    printf( "my height is %d m %4d cm \n",1,73);
+    printf( "my height is %d m %2d cm \n",1,73);
+    
+}
