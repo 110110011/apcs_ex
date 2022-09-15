0dd5a8ac309f115e07c8aff02a191e35295bf90c
110110011 <113594506+110110011@users.noreply.github.com>
2022-09-15 18:51:58 +0800

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{       
+    ;printf( "hello 王信元 \n" );
+    ;cout << "hello selena"
+    ;cout << "\nmy height is " << 1 << "m" << 72.5 << "cm"
+    
