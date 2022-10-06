f2a2aeab3d76ddd5223bf1a675a7d14c865a4af5
110110011 <113594506+110110011@users.noreply.github.com>
2022-10-06 18:06:28 +0800

    Create 16.cpp

---
+++ 16.cpp
@@ -0,0 +1,22 @@
+int main()
+{
+    int a,b,temp;
+    a = 60;
+    b = 60;
+    printf("please input a:");
+    scanf("%d" ,&a);
+    printf("please input b:");
+    scanf("%d" ,&b);
+    
+    if (a >= b)
+    {
+        printf ("a >= b \n");
+        
+    }else {
+        printf ("a < b \n");
+        
+    }
+    cout << "Hello World";
+    
+    return 0;
+}
