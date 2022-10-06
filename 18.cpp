05da12e25f5aa25d1d35dffd7e532d25f1756d9d
110110011 <113594506+110110011@users.noreply.github.com>
2022-10-06 18:51:20 +0800

    Create 18.cpp

---
+++ 18.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int  a, b, s, temp;
+   printf("input a:");
+   scanf("%d", &a);
+   printf("input b:");
+   scanf("%d", &b);
+   s = a + b;
+   printf("max = %d",s);
+        
+    
+   
+    
+    return 0;
+}
