527aa3984899eafb3ad5c9447565bf1535d26711
110110011 <113594506+110110011@users.noreply.github.com>
2022-10-06 17:48:13 +0800

    Create 15.cpp

---
+++ 15.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,temp;
+    a = 60;
+    b = 52;
+    if (a >= b)
+    {
+        printf ("a >= b \n");
+        
+    }else {
+        printf ("a < b \n");
+        
+    }
+    cout << "Hello World";
+    
+    return 0;
+}
