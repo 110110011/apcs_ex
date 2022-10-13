1f1c97222f9972990cf12228eff5eb36cba38c32
110110011 <113594506+110110011@users.noreply.github.com>
2022-10-13 18:43:47 +0800

    Create 20.cpp

---
+++ 20.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+int
+main ()
+{
+  int s, v;
+  printf ("please input a word :");
+  scanf ("%d", &s);s/=10;
+#if 0
+  if (s > 80)
+    {
+      printf ("your score level is A\n");
+    }
+  else if (s <= 80 && s > 70)
+    {
+      printf ("your score level is B\n");
+    }
+  else if (s <= 70 && s > 60)
+    {
+      printf ("your score level is C\n");
+    }
+  else if (s <= 60 && s > 40)
+    {
+      printf ("your score level is D\n");
+    }
+  else
+    {
+      printf ("your score level is E\n");
+    }
+#else
+
+  switch (s)
+  
+    {
+    case 10:
+      printf("your score level is A\n");
+      break;
+    case 9:
+      printf("your score level is A\n");
+      break;
+    case 8:
+      printf("your score level is A\n");
+      break;
+    case 7:
+      printf("your score level is B\n");
+      break;
+    case 6:
+      printf("your score level is C\n");
+      break;
+    case 5:
+      printf("your score level is D\n");
+      break; 
+    case 4:
+      printf("your score level is D\n");
+      break;
+    
+      
+     
+    
+        
+      
+
+      
+    default:;
+    printf("your score level is E\n");
+    
+    break;
+    }
+#endif
+  cout << "Hello World\n";
+  return 0;
+
+}
