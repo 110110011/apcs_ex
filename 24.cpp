3605e2bd8c5c6432fda1983c21c389437899d5d9
110110011 <113594506+110110011@users.noreply.github.com>
2022-10-27 18:31:05 +0800

    Create 24.cpp

---
+++ 24.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=1;i<10;i++){
+        // for(;;) {
+        printf("%d * %d = %d \n",1,i,1 * i);
+        //}
+        
+        }
+    int j;
+    for(j=1;j<10;j++){
+        //for(;;){
+        printf("%d * %d = %d \n",2,j,2 * j);
+        }
+    int k;
+    for(k=1;k<10;k++){
+        //for(;;){
+        printf("%d * %d = %d \n",3,k,3 * k);
+        }
+    int r;
+    for(r=1;r<10;r++){
+        //for(;;){
+        printf("%d * %d = %d \n",4,r,4 * r);
+        }
+    
+    
+    
+    return 0;
+}
