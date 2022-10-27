408a6dc16d726eb0461df8b63077f21e1a69ed04
110110011 <113594506+110110011@users.noreply.github.com>
2022-10-27 18:51:24 +0800

    Create 25.cpp

---
+++ 25.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int j;
+    for(i=1;i<10;i++){
+        for(j=1;j<10;j++){
+        // for(;;) {
+        printf("%d * %d = %d ",j,i,j * i);
+        //}
+        
+        }
+    printf("\n");
+    }    
+        
+    
+    
+    
+    return 0;
+}
