9bd16d0655f1a6a6e4f21ce869d7b82cebfe4a90
110110011 <113594506+110110011@users.noreply.github.com>
2022-11-03 19:47:44 +0800

    Create 60的因數.cpp

---
+++ 60的因數.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, i;
+    scanf("%d", &a); //q = 60
+    printf("60的因數: ");
+    for(i=1;i<a;i++){
+        if(a % i == 0){
+            printf(" %d ", i);
+        }
+    }
+    return 0;
+}
