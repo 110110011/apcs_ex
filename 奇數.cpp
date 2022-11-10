9fabae3d65911be0cd94fa9d11f2ad4d78680778
110110011 <113594506+110110011@users.noreply.github.com>
2022-11-10 19:10:39 +0800

    Create 奇數.cpp

---
+++ 奇數.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,sum = sum = 0 ;
+    
+    for(i=1;i<1000;i+=2)
+    {
+        sum = sum + i;
+        printf("%d %d \n", i,sum);
+        
+    
+    
+        
+    }
+    
+    
+    cout<<"Hello World";
+
+    return 0;
+}
