cd583fed65d10e3ba172cbc1f2c05c68a027b830
110110011 <113594506+110110011@users.noreply.github.com>
2023-01-05 18:54:50 +0800

    Create G.cpp

---
+++ G.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+void is_prime(int a,int b){
+    for(b=2;b<a;b++){
+        if(a % b == 0){
+        printf(" N \n");
+       break;
+        } 
+    }
+    if(b == a ){ 
+        printf(" Y \n"); 
+    };
+}
+
+int main()
+{
+int i,n,a,b;
+scanf("%d",&n);
+for( i=0;i<n;i++){
+    scanf("%d",&a); 
+    is_prime(a,b);
+    
+}
+return 0;
+}
